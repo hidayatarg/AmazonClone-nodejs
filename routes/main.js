5a5f199ba71aa7d10af0567dc9caeda4afe3606d
Hidayat arghandabi <hidayatarg@gmail.com>
2019-03-04 01:25:43 +0300

    Updated GET Category Method

--- routes/main.js
+++ routes/main.js
@@ -3,15 +3,18 @@ const Category = require('../models/category');
 
 router.route('/categories')
     .get((req, res, next) =>{
-        let category = new Category();
-        res.json({
-            success: true,
-            category: category,
-            message: 'All categories Successful'
-        });
+        // find all categories by mangoose
+        Category.find({}, (err, categories) => {
+            res.json({
+                success: true,
+                message: 'All categories Successful',
+                categories: categories
+            });
+        });  
     })
     .post((req, res, next) => {
         let category = new Category();
+        // it will take from the request form category
         category.name = req.body.category;
         category.save();
         res.json({
