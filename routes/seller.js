7f4974d582177b61604e864ce60ac79bc119af62
Hidayat Arghandabi <hidayatarg@gmail.com>
2019-03-09 17:12:49 +0300

    Update the bucket key

--- routes/seller.js
+++ routes/seller.js
@@ -5,8 +5,8 @@ const aws = require('aws-sdk');
 const multer = require('multer');
 const multerS3 = require('multer-s3');
 const s3 = new aws.S3({
-    accessKeyId: "AKIAIAOLULFJJLN3LMTA",
-    secretAccessKey: "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"
+    accessKeyId: "AKIAJ7JMZ3RTUCT3X7XQ",
+    secretAccessKey: "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"
 });
 
 const upload = multer({
